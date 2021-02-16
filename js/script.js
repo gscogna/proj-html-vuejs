b7be445d6be6ed173d5aa267ddac7c6f68b7c5e8
Gennaro <scognamiglioge@hotmail.it>
2021-02-16 21:47:33 +0100

    Ultima sezione del footer

--- js/script.js
+++ js/script.js
@@ -1,6 +1,22 @@
 var app = new Vue({
     el:'#app',
     data:{
-        logo:''
+        riquadri:[
+            {
+                foto: 'img/avadabarbers-second-blog-320x202.jpg',
+                titolo: 'Avada Barbers Now Open',
+                paragrafo: 'Il miglior posto per tagliare i capelli.'
+            },
+            {
+                foto: 'img/avadabarbers-choosing-blog-320x202.jpg',
+                titolo: 'Choosing The Right Barber',
+                paragrafo: 'Miglior comodità e prezzo.'
+            },
+            {
+                foto: 'img/avadabarbers-ourservice-blog-320x202.jpg',
+                titolo:'Quick Service Guide',
+                paragrafo: 'T facim a barb come barbanera il 4 imperatore.'
+            }
+        ]
     }
 });
